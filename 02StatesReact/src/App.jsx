5229276ac43a70b3ec7098b854fe65dcf4032880
M Safiullah Khan <m.safi.ullah@outlook.com>
2024-06-23 22:06:18 +0500

    Learned States

--- 02StatesReact/src/App.jsx
+++ 02StatesReact/src/App.jsx
@@ -1,12 +1,11 @@
 // import Counter from "./Counter.jsx";
 // import LikeButton from "./LikeButton.jsx";
 // import Ludo from "./Ludo.jsx";
-import Todo from "./Todo.jsx"
+// import Todo from "./Todo.jsx"
 
 function App() {
   return (
     <>
-      <Todo />
     </>
   );
 }
