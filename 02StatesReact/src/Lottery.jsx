5f98530e20564008f2303f26038888cf145a072f
safi-io <m.safi.ullah@outlook.com>
2024-06-24 12:24:06 +0500

    Done with user input target

--- 02StatesReact/src/Lottery.jsx
+++ 02StatesReact/src/Lottery.jsx
@@ -3,20 +3,30 @@ import { randomNum, sumRandomNum } from "./helperLottery.js";
 
 export default function Lottery() {
   let [arr, setArr] = useState(randomNum(3));
-  let isEqual = sumRandomNum(arr) === 10;
+  let [target, setTarget] = useState(0);
+  let isEqual = sumRandomNum(arr) === target;
 
   let genNew = () => {
     setArr(randomNum(3));
   };
 
+  let updateTarget = (e) => {
+    setTarget(Number(e.target.value));
+  }
+
 
   return (
     <>
+        <h1>Put Target</h1>
+      <input type="text" onChange={updateTarget} /> <br/>
+
+      <h1>Lottery Number</h1>
      
       <span>{arr[0]}</span>
       <span>{arr[1]}</span>
       <span>{arr[2]}</span>
-      <h3>{isEqual && "Congo"}</h3>
+
+      <h3>{isEqual && "Congratulations, you won"}</h3>
       <button onClick={genNew}>Replay</button>
     </>
   );
