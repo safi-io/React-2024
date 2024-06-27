6a36e95a591da047a05778cc29ece50a404d7fa4
safi-io <m.safi.ullah@outlook.com>
2024-06-27 04:52:25 -0700

    From New Operating Systems

--- 03bgChanger/src/App.jsx
+++ 03bgChanger/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <button className='text-white border-2 mx-5 p-5 rounded-xl' style={{backgroundColor: "pink"}} onClick={()=>(setcolor("pink"))}>PINK</button>
       <button className='text-white border-2 mx-5 p-5 rounded-xl' style={{backgroundColor: "black"}} onClick={()=>(setcolor("black"))}>BLACK</button>
       <button className='text-black border-2 mx-5 p-5 rounded-xl' style={{backgroundColor: "white"}} onClick={()=>(setcolor("white"))}>WHITE</button>
-      <button className='text-red-500 border-2 mx-5 p-5 rounded-xl' style={{backgroundColor: "yellow"}} onClick={()=>(setcolor("yellow"))}>YELLOW</button>
+      <button className='text-red-300 border-2 mx-5 p-5 rounded-xl' style={{backgroundColor: "yellow"}} onClick={()=>(setcolor("yellow"))}>YELLOW</button>
       <button className='text-black border-2 mx-5 p-5 rounded-xl' style={{backgroundColor: "lavender"}} onClick={()=>(setcolor("lavender"))}>LAVENDER</button>
     </div>
   )
